583ded6ae2a9c5cd2c4d3185dce34f4c8bf993b5
Madeline <arngepulp@users.noreply.github.com>
2026-03-18 02:26:02 -0700

    added api call and json dump for single pokemon
    added constants folder to define universal definitiosn such as type IDs and type labels, stage Ids ...

---
+++ consts.py
@@ -0,0 +1,62 @@
+## things used everywhere
+
+TYPES = {
+    1: "Grass",
+    2: "Fire",
+    3: "Water",
+    4: "Lightning",
+    5: "Psychic",
+    6: "Fighting",
+    7: "Darkness",
+    8: "Metal",
+    9: "Dragon",
+    10: "Colorless",
+}
+
+TYPE_IDS = {v: k for k, v in TYPES.items()}
+
+ENERGY_SYMBOLS = {
+    "{G}": "Grass",
+    "{R}": "Fire",
+    "{W}": "Water",
+    "{L}": "Lightning",
+    "{P}": "Psychic",
+    "{F}": "Fighting",
+    "{D}": "Darkness",
+    "{M}": "Metal",
+    "{N}": "Dragon",
+    "{C}": "Colorless",
+}
+
+STAGES = {
+    0: "Basic",
+    1: "Stage1",
+    2: "Stage2",
+}
+
+STAGE_IDS = {v: k for k, v in STAGES.items()}
+
+ENERGY_SYMBOLS = {
+    "{G}": "Grass",
+    "{R}": "Fire",
+    "{W}": "Water",
+    "{L}": "Lightning",
+    "{P}": "Psychic",
+    "{F}": "Fighting",
+    "{D}": "Darkness",
+    "{M}": "Metal",
+    "{N}": "Dragon",
+    "{C}": "Colorless",
+}
+
+# --- Rarities ---
+RARITIES = [
+    "One Diamond",
+    "Two Diamond",
+    "Three Diamond",
+    "Four Diamond",
+    "One Star",
+    "Two Star",
+    "Three Star",
+    "Crown",
+]
--- data/cards/scraping/card_scrape_dev.py
+++ data/cards/scraping/card_scrape_dev.py
@@ -1,8 +1,15 @@
 import asyncio
 from tcgdexsdk import TCGdex
+import json
+
+import sys
+sys.path.append('../../../')  
+
+from consts import STAGE_IDS
+
 
 async def get_one_pokemon(card_id):
-    sdk = TCGdex("en")
+    sdk = TCGdex("en") # initializes in english
     
     card = await sdk.card.get(card_id)
     
@@ -10,15 +17,73 @@ async def get_one_pokemon(card_id):
         print("unable to find card")
         return
 
-    # testing if i get anything cool!
-    print(card.name)
-    print(f"ID: {card.id}")
-    
-    hp = getattr(card, 'hp', 'N/A')
-    types = getattr(card, 'types', [])
+
     print(f"\nImage URL: {card.image}/high.png")
     
-    print(card.__dict__)
+    # get collection in to save to correct file
+    collection_id = card.id.split('-')[0]
+    
+    #card.weaknesses[0].type .val works to
+    # im gonna for loop this in case there is ever a cse where multiple weaknesses exist
+    weaknesses_list = []
+    weaknesses = getattr(card, 'weaknesses', [])
 
+    if weaknesses:
+        for w in weaknesses:
+            weaknesses_list.append({
+                "type": w.type,
+                "value": w.value
+            })
+    else:
+        weaknesses = None
+        
+        
+    data = {
+        "id": card.id, 
+        "name": card.name,
+        # get attr is safer, allows for default value if none is found good for dealing with other card types
+        "card_type": getattr(card, 'category', 'Pokemon'), 
+        "hp": getattr(card, 'hp', 0),
+        "types": getattr(card, 'types', []),
+    
+        "stage": STAGE_IDS[card.stage],
+        "evolves_from": getattr(card, 'evolveFrom', None),
+        
+        "abilities": [
+            {
+                "name": ability.name,
+                "effect_id": getattr(ability, 'effect', None)
+            } for ability in getattr(card, 'abilities', []) or []
+        ],
+        
+        "attacks": [
+            {
+                "name": attack.name,
+                "cost": getattr(attack, 'cost', []),
+                "damage": getattr(attack, 'damage', "0"), 
+                "effect_id": getattr(attack, 'effect', None) 
+            } for attack in getattr(card, 'attacks', []) or []
+        ],
+        
+        
+        "weakness": weaknesses_list,
+        "retreat_cost": getattr(card, 'retreat', 0),
+        "rarity": getattr(card, 'rarity', ""),
+        
+    }
+    
+    return data, collection_id
+    
+    
+    
+    
 if __name__ == "__main__":
-    asyncio.run(get_one_pokemon("A1-001"))
+    card_id = "A1-002"
+    card_data, collection_id = asyncio.run(get_one_pokemon(card_id=card_id))
+    
+    folder = "data/cards/"
+    fname = f"{folder}pokemon/{collection_id}/{card_id}.json"
+    with open(fname, "w") as file:
+        json.dump(card_data, file, indent=2)  
+    
+
